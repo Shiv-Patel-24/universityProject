4abaadca1002f0accc00721e2586b04049f07a61
Shiv-Patel-24 <pshiv2411@gmail.com>
2025-02-15 12:59:59 +0530

    created seveal files

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const mongoose = require("mongoose")
 const path = require("path")
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engin", "ejs");
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({extended : true}))
 app.use(methodOverride("_method"))
+app.engine("ejs", ejsMate);
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/finalproject';
 
